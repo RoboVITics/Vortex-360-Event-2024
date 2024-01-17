e524999c43315274aac67c8aeaea2e7361e5f557
nipun <nipunsnair2004@gmail.com>
2024-01-18 01:21:47 +0530

    Resolved the vercel issues

--- src/FAQs.js
+++ src/FAQs.js
@@ -1,107 +1,50 @@
-import React, { useRef } from "react";
-import { motion, useAnimation, useInView } from "framer-motion";
+import React from "react";
+
 const FAQs = () => {
-  const ref = useRef(null);
-  const inView = useInView(ref, { once: true });
-  const controls = useAnimation();
-  if (inView) {
-    controls.start("visible");
-  }
   return (
     <div id="faqs">
-      <div class="container px-4 py-5" id="featured-3">
-        <h2 class="pb-2 border-bottom">FAQs</h2>
+      <h2>FAQs</h2>
+      <div className="faqs">
+        <div className="cardcontainer">
+          <div className="flip">
+            <div className="front">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum,
+              voluptate?
+            </div>
+            <div className="back">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+              Repudiandae corporis rerum voluptatibus, at suscipit itaque ab
+              cumque aliquid omnis deleniti.
+            </div>
+          </div>
+        </div>
+        <div className="cardcontainer">
+          <div className="flip">
+            <div className="front">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum,
+              voluptate?
+            </div>
+            <div className="back">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+              Repudiandae corporis rerum voluptatibus, at suscipit itaque ab
+              cumque aliquid omnis deleniti.
+            </div>
+          </div>
+        </div>
+        <div className="cardcontainer">
+          <div className="flip">
+            <div className="front">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum,
+              voluptate?
+            </div>
+            <div className="back">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+              Repudiandae corporis rerum voluptatibus, at suscipit itaque ab
+              cumque aliquid omnis deleniti.
+            </div>
+          </div>
+        </div>
       </div>
-      <motion.div
-        className="faqs"
-        ref={ref}
-        variants={{
-          hidden: { scale: 0.6 },
-          visible: { scale: 1 },
-        }}
-        initial="hidden"
-        animate={controls}
-        transition={{ duration: 0.8 }}
-      >
-        <div className="cardcontainer">
-          <div className="flip">
-            <div className="front">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum,
-              voluptate?
-            </div>
-            <div className="back">
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-              Repudiandae corporis rerum voluptatibus, at suscipit itaque ab
-              cumque aliquid omnis deleniti.
-            </div>
-          </div>
-        </div>
-        <div className="cardcontainer">
-          <div className="flip">
-            <div className="front">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum,
-              voluptate?
-            </div>
-            <div className="back">
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-              Repudiandae corporis rerum voluptatibus, at suscipit itaque ab
-              cumque aliquid omnis deleniti.
-            </div>
-          </div>
-        </div>
-        <div className="cardcontainer">
-          <div className="flip">
-            <div className="front">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum,
-              voluptate?
-            </div>
-            <div className="back">
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-              Repudiandae corporis rerum voluptatibus, at suscipit itaque ab
-              cumque aliquid omnis deleniti.
-            </div>
-          </div>
-        </div>
-        <div className="cardcontainer">
-          <div className="flip">
-            <div className="front">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum,
-              voluptate?
-            </div>
-            <div className="back">
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-              Repudiandae corporis rerum voluptatibus, at suscipit itaque ab
-              cumque aliquid omnis deleniti.
-            </div>
-          </div>
-        </div>
-        <div className="cardcontainer">
-          <div className="flip">
-            <div className="front">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum,
-              voluptate?
-            </div>
-            <div className="back">
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-              Repudiandae corporis rerum voluptatibus, at suscipit itaque ab
-              cumque aliquid omnis deleniti.
-            </div>
-          </div>
-        </div>
-        <div className="cardcontainer">
-          <div className="flip">
-            <div className="front">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum,
-              voluptate?
-            </div>
-            <div className="back">
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-              Repudiandae corporis rerum voluptatibus, at suscipit itaque ab
-              cumque aliquid omnis deleniti.
-            </div>
-          </div>
-        </div>
-      </motion.div>
     </div>
   );
 };
--- src/Timeline.js
+++ src/Timeline.js
@@ -1,20 +1,13 @@
-import React, { useState, useEffect, useRef } from "react";
-import { motion, useAnimation, useInView } from "framer-motion";
+import React, { useState, useEffect } from "react";
 import "../src/Timeline.css";
 import timelineGif from "./timeline_gif.gif"; // Import the images here
 import gff from "./gff.jpg";
 
 const Timeline = () => {
-  const ref = useRef(null);
-  const inView = useInView(ref, { once: true });
-  const controls = useAnimation();
-  if (inView) {
-    controls.start("visible");
-  }
   const [currentIndex, setCurrentIndex] = useState(0);
 
   const imageAddresses = [
-    timelineGif,
+    gff,
     gff,
 
     // Add more image addresses as needed
@@ -45,21 +38,11 @@ const Timeline = () => {
 
   return (
     <div id="timeline">
-      <div className="container px-4 py-5">
-        <h2 className="border-bottom">Timeline</h2>
-      </div>
-      <motion.div
-        ref={ref}
-        variants={{
-          hidden: { scale: 0.6 },
-          visible: { scale: 1 },
-        }}
-        initial="hidden"
-        animate={controls}
-        transition={{ duration: 0.8 }}
-      >
-        <img src="https://picsum.photos/1000" />
-      </motion.div>
+      <h2>Timeline</h2>
+      <img
+        src={imageAddresses[currentIndex]}
+        alt={imageAddresses[currentIndex]}
+      />
     </div>
   );
 };
