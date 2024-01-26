b3ca102592f3c6206a5727be55905a2ad078a938
Tanaynaik07~ <tanaynaik0710@gmail.com>
2024-01-27 01:09:58 +0530

    Initiated Teams page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Main from "./Main";
 import { Route, Routes } from "react-router-dom";
 import DomainInfo from "./components/Landing-Page/DomainInfo";
 import Progressbar from "./components/Landing-Page/Progressbar";
+import Teams from "../src/components/Teams/Teams";
 
 function App() {
   const [a, setA] = useState(100);
@@ -27,6 +28,7 @@ function App() {
           element={loading ? <Progressbar value={100 - a} /> : <Main />}
         ></Route>
         <Route path="/domain/:domainId" element={<DomainInfo />}></Route>
+        <Route path="/teams" element={<Teams />}></Route>
       </Routes>
     </>
   );
