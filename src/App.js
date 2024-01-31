c5deb4ca8331af9d4ad562f39aed40d667707310
Nipun <nipunsnair2004@gmail.com>
2024-01-31 12:23:34 +0530

    Hero section commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import "./App.css";
-import { useEffect, useState } from "react";
-import Main from "./Main";
 import { Route, Routes, useNavigate, useLocation } from "react-router-dom";
 import SideBar from "./components/Sidebar/SideBar";
 import DomainInfo from "./components/Landing-Page/DomainInfo";
 import CreateProfile from "./components/Profile/CProfile";
 import Profile from "./components/Profile/Profile";
-import Progressbar from "./components/Landing-Page/Progressbar";
 import TeamRegistration from "../src/components/Teams/TeamRegistration";
 import Submissions from "./components/Submission-Page/Submission";
 import Login from "./components/Login/Login";
@@ -15,24 +12,14 @@ import Dashboard from "./components/Dashboard/Dashboard";
 import Submitted from "./components/Submission-Page/Submittedform"
 import TeamProfile from "./components/Teams/TeamProfile";
 import Teams from "./components/Teams/Teams";
+import LandingPage from "./components/Landing-Page/LandingPage";
+
 
 
 function App() {
-  const [a, setA] = useState(100);
-  const [loading, setLoading] = useState(true);
   const navigate = useNavigate();
   const location = useLocation();
-  useEffect(() => {
-    if (a) {
-      setTimeout(() => {
-        setA(a - 1);
-      }, 20);
-    } else {
-      setTimeout(() => {
-        setLoading(false);
-      }, 1500);
-    }
-  }, [a]);
+
   const shouldShowSidebar = () => {
     const hiddenPaths = ["/", "/login", "/register"];
     return !hiddenPaths.includes(location.pathname);
@@ -41,25 +28,20 @@ function App() {
     <>
       <div style={{ display: "flex" }}>
         {shouldShowSidebar() && <SideBar />}
-        <div
-          style={{ marginLeft: shouldShowSidebar() ? "30px" : "0", flex: 1 }}
-        >
+        <div>
           <Routes>
             <Route
-              path="/"
-              element={loading ? <Progressbar value={100 - a} /> : <Main />}
+              path="/" element={<LandingPage/>}
             />
             <Route path="/domain/:domainId" element={<DomainInfo />} />
             <Route path="/teams" element={<Teams />} />
-
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
             <Route path="/profile" element={<CreateProfile />} />
             <Route path="/editprofile" element={<Profile />} />
             <Route path="/teamreg" element={<TeamRegistration />} />
-
             <Route path="/submissions" element={<Submissions />} />
             <Route path="/submittedform" element={<Submitted />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
             <Route path="/dashboard" element={<Dashboard />} />
             <Route path="/teamprofile" element={<TeamProfile/>}/>
           </Routes>
---
+++ src/components/Landing-Page/Main.js
@@ -0,0 +1,32 @@
+import Navbar from "./Navbar";
+import Domains from "./Domains";
+import Timeline1 from "./Timeline";
+import Sponsors from "./Sponsors";
+import Prizes from "./Prizes";
+import FAQs from "./FAQs";
+import Contact from "./Contact";
+import React from "react";
+import Hero from "./Hero";
+import Timer from "./Timer";
+import Mouse from "./Mouse";
+import Overlay from "./Overlay";
+
+const Main = () => {
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+      <Domains />
+      <Timeline1 />
+      <Sponsors />
+      <Prizes />
+      <FAQs />
+      <Timer />
+      <Contact />
+      <Mouse />
+      <Overlay />
+    </div>
+  );
+};
+
+export default Main;
